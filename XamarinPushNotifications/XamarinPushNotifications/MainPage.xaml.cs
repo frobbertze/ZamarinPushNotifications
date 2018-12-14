774d21af78c3a6b45d06e2b079085cfab0ab7883
Francois Robbertze <frobbertze@gmail.com>
2018-12-14 11:16:49 +0200

    Add project files.

---
+++ XamarinPushNotifications/XamarinPushNotifications/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinPushNotifications
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            LoadButton.Clicked += LoadButton_Clicked;
+            SaveButton.Clicked += SaveButton_Clicked;
+        }
+
+        private void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            Application.Current.Properties["valuesaved"] = Valuetosave.Text;
+        }
+
+        private void LoadButton_Clicked(object sender, EventArgs e)
+        {
+            Valuetosave.Text = Application.Current.Properties["valuesaved"].ToString();
+        }
+    }
+}
